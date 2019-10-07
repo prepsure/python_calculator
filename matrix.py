3b70bfb8c569404434ce64ef94b6f56ab0664031
PressurizedSphere <33643911+PressurizedSphere@users.noreply.github.com>
2019-10-06 22:58:47 -0400

    added 2 scripts - matrix and vector
    vector has nothing in it
    matrix has 3 public functions rref, totalsum, and det

---
+++ matrix.py
@@ -0,0 +1,57 @@
+print("split lists using commas")
+print("""
+ the functions in this module are:
+  totalsum, det, rref
+""")
+
+def _enter_matrix():
+  return [[float(i) for i in input("Enter Row of Matrix: ").split(",")] for j in range(int(input("Number of Rows: ")))]
+
+def totalsum():
+  m = _enter_matrix()
+  r = 0
+  for i in range(len(m)):
+    for j in range(len(m[i])):
+      r += m[i][j]
+      m[i][j] = r
+  print(m)
+
+# works for 2 x 2 and 3 x 3 matrices
+def det():
+  print("row must be 2 or 3")
+  m = _enter_matrix()
+  r = 0
+  if len(m) == 2:
+    r = m[0][0]*m[1][1] - m[0][1]*m[1][0]
+  elif len(m) == 3:
+    r = m[0][0]*(m[1][1]*m[2][2] - m[1][2]*m[2][1]) - m[0][1]*(m[1][0]*m[2][2] - m[2][0]*m[1][2]) + m[0][2]*(m[1][0]*m[2][1] - m[1][1]*m[2][0])
+  print(r)
+
+def rref():
+  m = _enter_matrix()
+
+  if not m: return
+  l = 0
+  rC = len(m)
+  cC = len(m[0])
+  for r in range(rC):
+      if l >= cC:
+          return
+      i = r
+      while m[i][l] == 0:
+          i += 1
+          if i == rC:
+              i = r
+              l += 1
+              if cC == l: return
+      m[i], m[r] = m[r], m[i]
+      lv = m[r][l]
+      m[r] = [mrx / float(lv) for mrx in m[r]]
+      for i in range(rC):
+          if i != r:
+              lv = m[i][l]
+              m[i] = [ iv - lv*rv for rv,iv in zip(m[r], m[i])]
+      l += 1
+  
+  for rw in m:
+    print((",").join((str(rv) for rv in rw)))
