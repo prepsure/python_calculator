777a153eac2fa07d525fa8d6cf34455637f48667
PressurizedSphere <33643911+PressurizedSphere@users.noreply.github.com>
2019-10-07 22:13:06 -0400

    added vector script
    it has cross, dot, and conversions of vectors

---
+++ vector.py
@@ -0,0 +1,54 @@
+from math import pi, radians, cos, sin, atan2, sqrt
+
+# takes a vector in unit from or magnitude and direction form
+# returns a vector in unit vector form
+def __enter_vector():
+    print("press 1 for unit vector\n2 for mag and direction")
+    choice = input("press 1 for unit vector\n2 for mag and direction:\n")
+    if choice == "1":
+        return __enter_unit_vector()
+    else:
+        return __enter_mag_and_angle()
+
+def __enter_unit_vector():
+    vector = []
+    for n in input("enter vector: ").split(","):
+        vector.append(float(n))
+    return vector
+
+def __enter_mag_and_angle():
+    angle = float(input("enter angle: "))
+    mag = float(input("enter magnitude: "))
+
+    if angle >= 2 * pi or angle <= -2 * pi:
+        angle = radians(angle)
+        print("assuming degrees")
+    else:
+        print("assuming radians")
+
+    return [mag * cos(angle), mag * sin(angle)]
+
+def to_unit():
+    return __enter_mag_and_angle()
+
+def to_mag_and_direction():
+    v = __enter_unit_vector()
+
+    print("angle: " + atan2(v[1], v[0]))
+    print("magnitude: " + sqrt(v[0]**2 + v[1]**2))
+
+def cross():
+    v1 = __enter_unit_vector()
+    v2 = __enter_unit_vector()
+
+    crossed = [v1[1] * v2[2] - v1[2] * v2[1], v1[2] * v2[0] - v1[0] * v2[2], v1[0] * v2[1] - v1[1] * v2[0]]
+    
+    print(crossed)
+
+def dot():
+    v1 = __enter_unit_vector()
+    v2 = __enter_unit_vector()
+
+    dotted = sum(v1[i] * v2[i] for i in range(max(len(v1), len(v2))))
+
+    print(dotted)
