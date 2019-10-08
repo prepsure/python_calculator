db065468372ceb81ebc7fdc50c3d39c27568e582
PressurizedSphere <33643911+PressurizedSphere@users.noreply.github.com>
2019-10-08 16:44:23 -0400

    changed evaluated_types to __evaluated_types

--- input_handler.py
+++ input_handler.py
@@ -1,10 +1,10 @@
-evaluated_types = [
+__evaluated_types = [
     int, float, complex, hex, oct
 ]
 
 # out is the message right before the input, t is the type the input should be cast as
 def __input_eval(out, t):
     data = input(out)
-    if t in evaluated_types:
+    if t in __evaluated_types:
         data = eval(data)
     return t(data)
