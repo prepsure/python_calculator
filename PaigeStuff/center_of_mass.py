f9b45b0556c6b97777309f66a0cc7db796070293
AtwanZ <33740557+AtwanZ@users.noreply.github.com>
2019-10-28 20:00:58 -0400

    Update center_of_mass.py

--- PaigeStuff/center_of_mass.py
+++ PaigeStuff/center_of_mass.py
@@ -1,10 +1,9 @@
 from input_handler import __input_eval as enput, __input_list as enput_list
 
-def __combine_com(dense = False):
+def center_of_mass():
     num_objs = enput("how many objects? ", int)
     objs_coords = []
     objs_mass = []
-    objs_dense = []
 
     for i in range(0, num_objs):
         coords = enput_list("coordinates of object: ", float)
@@ -21,10 +20,24 @@ def __combine_com(dense = False):
         cm /= total_mass
         print("cm: " + str(cm))
 
-def center_of_mass():
-    __combine_com()
-    #unfinished
-
 def center_of_dense_mass():
-    __combine_com
-    #unfinished
+    num_objs = enput("how many objects? ", int)
+    objs_coords = []
+    objs_mass = []
+
+    for i in range(0, num_objs):
+        coords = enput_list("coordinates of object: ", float)
+        volume = enput("volume of object: ", float)
+        density = enput("density of object", float)
+        mass = density * volume
+        objs_coords.append(coords)
+        objs_mass.append(mass)
+
+    for coord_place in range(0, len(objs_coords[0])):
+        cm = 0
+        total_mass = 0
+        for obj_place in range(0, len(objs_coords)):
+            cm += objs_mass[obj_place] * objs_coords[obj_place][coord_place]
+            total_mass += objs_mass[obj_place]
+        cm /= total_mass
+        print("cm: " + str(cm))
