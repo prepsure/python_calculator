e251781675d1a2cfcc81a2f6bdb5e1b9017ab349
JarnaChao09 <yunyang4928@gmail.com>
2019-11-10 19:02:02 -0500

    Using input_handler

--- JaranStuff/physics.py
+++ JaranStuff/physics.py
@@ -2,14 +2,14 @@ from input_handler import __input_eval as enput, __input_list_list_eval as listp
 
 
 def work_from_velo():
-    mass = float(input("Enter Mass in (kg): "))
-    velocity = [float(i) for i in input("Enter Velocity Vf Vi in (m/s):\n").split(" ")]
+    mass = enput("Enter Mass in (kg):", float)
+    velocity = listput("Enter Velocity Vf Vi in (m/s):", float)
     return 1/2 * mass * (velocity[0]**2 - velocity[1]**2)
 
 
 def work_from_x():
-    kx = float(input("Enter Spring Const: "))
-    displacement = [float(i) for i in input("Enter Position Xi Xf in (m):\n").split(" ")]
+    kx = enput("Enter Spring Const:", float)
+    displacement = listput("Enter Position Xi Xf in (m)", float)
     return 1/2 * kx * (displacement[0]**2 - displacement[1]**2)
 
 
@@ -17,6 +17,5 @@ def center_of_mass():
     coordinates_of_particles = listput("Enter M + coordinates of Particle", "Enter Number of Particles", float)
     cop = coordinates_of_particles.copy()
     sum_of_mass = sum(cop[i][0] for i in range(len(cop)))
-    return [sum([cop[j][0] * cop[j][i] for j in range(len(cop))])/sum_of_mass for i in range(1,len(cop[0]))]
-
+    return [sum([cop[j][0] * cop[j][i] for j in range(len(cop))])/sum_of_mass for i in range(1, len(cop[0]))]
 
